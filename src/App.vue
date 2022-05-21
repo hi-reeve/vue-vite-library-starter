445929570510f165e2e5017d4adfe6a81d1b0482
Christopher Reeve <reeveworkmail@gmail.com>
2022-05-21 15:55:56 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue';
+</script>
+
+<template>
+	<div class="wrapper">
+		<hello-world msg="You did it!" />
+	</div>
+</template>
+
+<style></style>
---
+++ vite.config.ts
@@ -0,0 +1,33 @@
+import { fileURLToPath, URL } from 'url';
+
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [vue(), vueJsx()],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+		},
+	},
+	build: {
+		lib: {
+			entry: path.resolve(__dirname, 'src/components/index.js'),
+			name: 'MyLib',
+			fileName: format => `my-lib.${format}.js`,
+		},
+		rollupOptions: {
+			external: ['vue'],
+			output: {
+				// Provide global variables to use in the UMD build
+				// Add external deps here
+				globals: {
+					vue: 'Vue',
+				},
+			},
+		},
+	},
+});
